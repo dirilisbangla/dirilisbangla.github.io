dca76eba9631e22b633bba1a4bf9c954752e04e8
araihanazizul <65993483+araihanazizul@users.noreply.github.com>
2020-06-15 23:10:01 +0600

    scroll top

---
+++ pages/pa/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+$("#topcontrol").click(function(){
+    $("html,body").animate({scrollTop:0},2000);
+    
+});
+
+
+});
---
+++ pages/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$("#topcontrol").click(function(){
+    $("html,body").animate({scrollTop:0},2000);
+});
+
+
+});
